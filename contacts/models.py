d8aa5ef031199a972e180659c3e45974da19acc9
kolgeldieva_a@iuca.kg <kolgeldieva_a@iuca.kg>
2025-11-03 17:17:06 +0600

    second commit

--- contacts/models.py
+++ contacts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Contact(models.Model):
-    laste_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50, blank=True, null=True)
     phone = models.CharField(max_length=20)
